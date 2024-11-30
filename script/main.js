3276decb36ed9b78ae18e8cedba706570cd3ec78
Jorge Sala <jorge.e.sala@gmail.com>
2024-11-30 11:07:58 -0500

    added login script

---
+++ script/main.js
@@ -0,0 +1,30 @@
+const API_URL = "https://task4-back-ru6z.onrender.com/api";
+console.log(document.getElementById("loginForm"));
+document.getElementById("loginForm").addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+    login(email, password);
+});
+
+
+async function login (email, password) {
+    const credentials = JSON.stringify({email, password});
+    const LOGIN_ENDPOINT = "/auth/login";
+
+    const response = await fetch(API_URL + LOGIN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+        },
+        body: credentials,
+    });
+
+    const data = await response.json();
+
+    if(response.ok){
+        alert("Logged in");
+    }else {
+        alert('Error: ' +  data.message);
+    }
+}
